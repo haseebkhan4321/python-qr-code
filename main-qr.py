3309c19a8edd70bee36f169496189fe3f33de421
Haseeb khan <haseeb.khan@ozteckwork.com>
2025-02-01 20:15:01 +0500

    first push

---
+++ main-qr.py
@@ -0,0 +1,23 @@
+import qrcode
+from PIL import Image
+app_link = "https://apps.apple.com/pk/app/wisemarket/id6474654022"
+logo_path = "wm4.png"  # Replace with your actual logo path
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,  # Allows for more error correction
+    box_size=10,
+    border=4,
+)
+qr.add_data(app_link)
+qr.make(fit=True)
+qr_img = qr.make_image(fill="black", back_color="white").convert("RGB")
+logo = Image.open(logo_path)
+logo = logo.convert("RGBA")
+logo_size = int(qr_img.size[0] * 0.25)  # 25% of the QR code size
+logo = logo.resize((logo_size, logo_size), Image.LANCZOS)
+logo_position = (
+    (qr_img.size[0] - logo.size[0]) // 2,
+    (qr_img.size[1] - logo.size[1]) // 2,
+)
+qr_img.paste(logo, logo_position, mask=logo.split()[3])  # Use the alpha channel as mask
+qr_img.save("wisemarket_mobile_ios_app_qr.png")
